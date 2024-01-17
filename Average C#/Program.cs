b9121fd056db9e4691fad950e0729f096860dfa6
ThogoGakinya <thogosamson@gmail.com>
2024-01-17 18:02:58 +0300

    Add project files.

---
+++ Average C#/Program.cs
@@ -0,0 +1,40 @@
+﻿namespace Average_C_
+{
+    internal class Program
+    {
+        static int total;
+        static int counter;
+        static int currentNumber;
+        static string? marks;
+        static void Main(string[] args)
+        {
+            Average();
+        }
+        public static void Average()
+        {
+           for(int counter = 0; counter < 20; counter++) 
+            {
+                Console.WriteLine("Please Enter Marks");
+                string marks = Console.ReadLine();
+                bool marksChecker = int.TryParse(marks, out currentNumber);
+
+                if (marksChecker) 
+                {
+                    if(currentNumber == -1) 
+                    {
+                        int average = total / counter;
+                        Console.WriteLine("The Average is {0} ", average);
+                        break;
+                    }
+                    total += currentNumber;
+                }
+                else
+                { 
+                    Console.WriteLine("Please Enter an Number");
+                    counter--;
+                }
+            }
+           
+        }
+    }
+}
